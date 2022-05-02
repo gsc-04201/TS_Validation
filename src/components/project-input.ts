502b4416b90594ba33716adc1f4c598cd2db8f13
SuenagaKeiya <keiya04201@gmail.com>
2022-05-02 14:18:53 +0900

    namespace&module

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,80 +1,82 @@
-/// <reference path="base-component.ts" />
+import Cmp from "./base-component.js";
+import * as Validation from "../util/validation.js"
+//名前の衝突を避ける際にasを使用する
+import { autobind as Autobind } from "../decorators/autobind.js";
+import { projectState } from "../state/project-state.js";
 
-namespace App {
-  // ProjectInput Class
-  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLInputElement;
-    mandayInputElement: HTMLInputElement;
-  
-    constructor() {
-      super('project-input', 'app', true, 'user-input')
-      
-  
-      // フォーム入力のinputエレメント
-      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-      this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
-      
-      this.configure();//プライベートだからクラスの内側でしかアクセスできない
+// ProjectInput Class
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
+
+  constructor() {
+    super('project-input', 'app', true, 'user-input')
+    
+
+    // フォーム入力のinputエレメント
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
+    
+    this.configure();//プライベートだからクラスの内側でしかアクセスできない
+  }
+
+  public configure() {
+    this.element.addEventListener('submit', this.submitHandler);//フォームがサブミットされた時にthis.submitHandlerが呼び出される 
+  }
+
+  renderContent(): void {
+    
+  }
+
+  private gatherUserInput(): [string, string, number] | void {//ユーザーの入力値を取得してバリデーション
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredManday = this.mandayInputElement.value;
+
+    const titleValidatable: Validation.Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validation.Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
     }
-  
-    public configure() {
-      this.element.addEventListener('submit', this.submitHandler);//フォームがサブミットされた時にthis.submitHandlerが呼び出される 
+    const mandayValidatable: Validation.Validatable = {
+      value: +enteredManday,
+      required: true,
+      min: 1,
+      max: 1000
     }
-  
-    renderContent(): void {
-      
+    if (//各項目が空白で無い場合
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(mandayValidatable)
+    ) {
+      alert('入力値が正しくありません。再度お試しください。')
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredManday];
     }
-  
-    private gatherUserInput(): [string, string, number] | void {//ユーザーの入力値を取得してバリデーション
-      const enteredTitle = this.titleInputElement.value;
-      const enteredDescription = this.descriptionInputElement.value;
-      const enteredManday = this.mandayInputElement.value;
-  
-      const titleValidatable: Validatable = {
-        value: enteredTitle,
-        required: true,
-      };
-      const descriptionValidatable: Validatable = {
-        value: enteredDescription,
-        required: true,
-        minLength: 5,
-      }
-      const mandayValidatable: Validatable = {
-        value: +enteredManday,
-        required: true,
-        min: 1,
-        max: 1000
-      }
-      if (//各項目が空白で無い場合
-        !validate(titleValidatable) ||
-        !validate(descriptionValidatable) ||
-        !validate(mandayValidatable)
-      ) {
-        alert('入力値が正しくありません。再度お試しください。')
-        return;
-      } else {
-        return [enteredTitle, enteredDescription, +enteredManday];
-      }
-    }
-  
-    private clearInputs() {
-      this.titleInputElement.value = '';
-      this.descriptionInputElement.value = '';
-      this.mandayInputElement.value = '';
-    }
-  
-    @autobind
-    // レシーバ関数この中で入力項目にアクセスして検証する
-    private submitHandler(event: Event) {
-      event.preventDefault();//httpリクエストが送られないようにする
-      const userInput = this.gatherUserInput();
-      if (Array.isArray(userInput)) {//配列？
-        const [title, desc, manday] = userInput;
-        projectState.addProject(title, desc, manday);
-        this.clearInputs();
-      }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.mandayInputElement.value = '';
+  }
+
+  @Autobind
+  // レシーバ関数この中で入力項目にアクセスして検証する
+  private submitHandler(event: Event) {
+    event.preventDefault();//httpリクエストが送られないようにする
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {//配列？
+      const [title, desc, manday] = userInput;
+      projectState.addProject(title, desc, manday);
+      this.clearInputs();
     }
   }
 }
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,47 +1,50 @@
-namespace App {
-  // ProjectItem Class
-  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
-    implements Draggable { //一つ一つの項目をリストのアイテムとして表示するためのクラス
-    private project: Project;
-  
-    get manday() {
-      if (this.project.manday < 20) {
-        return this.project.manday.toString() + '人日';
-      }
-      return (this.project.manday / 20).toString() + '人月'
+import { Draggable } from '../models/drag-drop.js'
+import { Project } from '../models/project.js';
+import Component from './base-component.js';
+import { autobind } from '../decorators/autobind.js';
+
+// ProjectItem Class
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable { //一つ一つの項目をリストのアイテムとして表示するためのクラス
+  private project: Project;
+
+  get manday() {
+    if (this.project.manday < 20) {
+      return this.project.manday.toString() + '人日';
     }
-  
-    constructor(hostId: string, project:Project) {
-      super("single-project", hostId, false, project.id);
-      this.project = project;
-  
-      this.configure()
-      this.renderContent();
-      
-    }
-  
-    @autobind
-    dragStartHandler(event: DragEvent) {
-      //drageventでデータを転送するためのプロパティ
-      event.dataTransfer!.setData('text/plain', this.project.id);
-      //ブラウザ上でカーソルがどのように表示されるかをコントロールするためのもの
-      event.dataTransfer!.effectAllowed = 'move';
-    }
-  
-    dragEndHandler(_: DragEvent){
-      console.log("Drag終了");
-    }
-  
-    configure() { 
-      this.element.addEventListener('dragstart', this.dragStartHandler);
-      this.element.addEventListener('dragend', this.dragEndHandler);
-    }
-    
-    renderContent() {
-      this.element.querySelector('h2')!.textContent = this.project.title;
-      this.element.querySelector('h3')!.textContent = this.manday;
-      this.element.querySelector('p')!.textContent = this.project.description;
-      console.log(this.project.description);
-      }
+    return (this.project.manday / 20).toString() + '人月'
   }
+
+  constructor(hostId: string, project:Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure()
+    this.renderContent();
+    
+  }
+
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    //drageventでデータを転送するためのプロパティ
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    //ブラウザ上でカーソルがどのように表示されるかをコントロールするためのもの
+    event.dataTransfer!.effectAllowed = 'move';
+  }
+
+  dragEndHandler(_: DragEvent){
+    console.log("Drag終了");
+  }
+
+  configure() { 
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
+  
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.manday;
+    this.element.querySelector('p')!.textContent = this.project.description;
+    console.log(this.project.description);
+    }
 }
